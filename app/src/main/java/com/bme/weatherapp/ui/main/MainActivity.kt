ae2d417986323d3e466475088f0d8abe22052515
p.gal <galpatrik13@gmail.com>
2023-05-10 13:20:53 +0200

    update

--- app/src/main/java/com/bme/weatherapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/bme/weatherapp/ui/main/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.annotation.VisibleForTesting
 import com.bme.weatherapp.persistence.AppDatabase
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
